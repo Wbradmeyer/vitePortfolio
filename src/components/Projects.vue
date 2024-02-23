f90a6b744e3f47dcf58e324d8d14100162439331
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-23 11:03:17 -0500

    instrument project add

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,9 +2,15 @@
 
 <template>
     <h1>Projects</h1>
+
     <h2>Quiz Show</h2>
     <a href="https://github.com/Wbradmeyer/quiz_show" target="_blank">
         <img src="../assets/quiz_show.png" alt="quiz show screenshot" class="project pic">
+    </a>
+
+    <h2>Instrument Hub</h2>
+    <a href="https://github.com/Wbradmeyer/MeyerSoloProject" target="_blank">
+        <img src="../assets/instrument_hub.png" alt="instrument hub screenshot" class="project pic">
     </a>
 </template>
 
