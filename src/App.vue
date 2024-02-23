783fc231343d36be9d815744cefb20ae503ff2b2
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-23 09:30:47 -0500

    fix work component import error

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue';
 import AboutMe from './components/AboutMe.vue';
-import WorkExperience from '.components/WorkExperience.vue';
+import WorkExperience from './components/WorkExperience.vue';
 import Contact from './components/Contact.vue';
 </script>
 
