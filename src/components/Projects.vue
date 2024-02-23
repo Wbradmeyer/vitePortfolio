fa4d437a5075e9b4b986cc315cac530c07123708
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-23 10:03:08 -0500

    pic error

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,7 +4,7 @@
     <h1>Projects</h1>
     <h2>Quiz Show</h2>
     <a href="https://github.com/Wbradmeyer/quiz_show" target="_blank">
-        <img src="./assets/quiz_show.png" alt="quiz show screenshot" class="project pic">
+        <img src="./src/assets/quiz_show.png" alt="quiz show screenshot" class="project pic">
     </a>
 </template>
 
