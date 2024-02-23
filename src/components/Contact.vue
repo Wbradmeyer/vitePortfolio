209fb6a361445cac11b5f61b84ff6de00dec849b
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-23 14:23:54 -0500

    contact form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,6 +5,19 @@
     <p class="contact">
         This will be a form to contact me.
     </p>
+    <form action="#">
+        <label>Your Name: {{ name }}</label>
+        <input v-model="name" placeholder="Jane Doe">
+
+        <label>Your Email: {{ email }}</label>
+        <input v-model="email" placeholder="janedoe@gmail.com">
+
+        <span>Message:</span>
+        <p>{{ message }}</p>
+        <textarea v-model="message" placeholder="Add your message" cols="30" rows="10"></textarea>
+
+        <button type="submit">Send</button>
+    </form>
 </template>
 
 <style>
