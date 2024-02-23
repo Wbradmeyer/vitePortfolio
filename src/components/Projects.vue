ad8d549cbe06d79a67d18f201543fb6f1832c644
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-23 10:00:40 -0500

    quiz project image

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,9 +3,13 @@
 <template>
     <h1>Projects</h1>
     <h2>Quiz Show</h2>
-    <a href="https://github.com/Wbradmeyer/quiz_show" target="_blank">Click Here</a>
+    <a href="https://github.com/Wbradmeyer/quiz_show" target="_blank">
+        <img src="./assets/quiz_show.png" alt="quiz show screenshot" class="project pic">
+    </a>
 </template>
 
 <style>
-    
+    .project.pic {
+        border-radius: 5px;
+    }
 </style>
