927b28fdd5d002b9620a9a12dc65dbbb8d864e0c
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-23 20:44:00 -0500

    contact form input style

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -7,21 +7,35 @@
     </p>
     <form action="#">
         <label>Your Name: {{ name }}</label>
-        <input v-model="name" placeholder="Jane Doe">
+        <input v-model="name" placeholder="Jane Doe" class="input">
 
         <label>Your Email: {{ email }}</label>
-        <input v-model="email" placeholder="janedoe@gmail.com">
+        <input v-model="email" placeholder="janedoe@gmail.com" class="input">
 
         <span>Message:</span>
         <p>{{ message }}</p>
-        <textarea v-model="message" placeholder="Add your message" cols="30" rows="10"></textarea>
+        <textarea v-model="message" placeholder="Add your message" cols="30" rows="10" class="input"></textarea>
 
-        <button type="submit">Send</button>
+        <button class="submit" type="submit">Send</button>
     </form>
 </template>
 
 <style>
     .contact {
         color: blue;
+    }
+
+    form {
+        display: block;
+    }
+
+    .input {
+        border: 2px solid darkslategray;
+        border-radius: 3px;
+    }
+
+    .submit {
+        color: white;
+        background-color: dodgerblue;
     }
 </style>
