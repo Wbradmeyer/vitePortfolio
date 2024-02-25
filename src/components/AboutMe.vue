357204632f30e2cdcf16c5a1e79d3fcbd0344baa
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-24 23:19:33 -0500

    add about me text

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -6,7 +6,17 @@
 <template>
     <h1>About Me</h1>
     <p class="about">
-        This is a blurb about me.
+        I am a full-stack developer and a music educator. I enjoy problem-solving and breaking down 
+        information into manageable parts to benefit others and myself. I believe life-long learning 
+        is essential to working a job and for living a rewarding life in general. Most recently I have 
+        been learning the Django web framework as well as bass guitar. As a 17-year educator, I have a 
+        wealth of experience in planning, collaborating with others, and mentoring.
+
+        I am proficient in Python (Flask and Django), Java (Spring Boot), and JavaScript (React) and have 
+        two music degrees. I really enjoy both disciplines as they blend together problem-solving, creativity, 
+        logic, and syntax. Whether working with front-end or back-end, or writing or rehearsing a song, the 
+        build is fun. Both coding and music have allowed me to meet and work with some unique, amazing, 
+        inspiring people.
     </p>
 
 </template>
