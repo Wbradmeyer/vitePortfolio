0b5dc77a578c553cbf2a911165370d9f5d930e77
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-25 14:19:59 -0500

    fix and widen nav

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,15 +6,12 @@ import { ref } from 'vue'
 <template>
   <div class="nav">
     <div>
-      <h1>Brad Meyer</h1>
-      <h2>Full-Stack Developer</h2>
+      <h1>Brad Meyer | Full-Stack Developer</h1>
     </div>
     <div>
       <ul class="nav_links">
         <li class="link">About Me</li>
         <li class="link">Work History</li>
-      </ul>
-      <ul class="nav_links">
         <li class="link">Projects</li>
         <li class="link">Contact</li>
       </ul>
@@ -24,14 +21,16 @@ import { ref } from 'vue'
 
 <style scoped>
 .nav {
+  max-width: 1200px;
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+  position: fixed;
 }
 
 .nav_links {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: baseline;
   list-style-type: none;
 }
