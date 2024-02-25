c9078ee2aeea759ada40496f99beadfda93ac35e
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-25 13:46:34 -0500

    about me box

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -5,24 +5,32 @@
 
 <template>
     <h1>About Me</h1>
-    <p class="about">
-        I am a full-stack developer and a music educator. I enjoy problem-solving and breaking down 
-        information into manageable parts to benefit others and myself. I believe life-long learning 
-        is essential to working a job and for living a rewarding life in general. Most recently I have 
-        been learning the Django web framework as well as bass guitar. As a 17-year educator, I have a 
-        wealth of experience in planning, collaborating with others, and mentoring.
-
-        I am proficient in Python (Flask and Django), Java (Spring Boot), and JavaScript (React) and have 
-        two music degrees. I really enjoy both disciplines as they blend together problem-solving, creativity, 
-        logic, and syntax. Whether working with front-end or back-end, or writing or rehearsing a song, the 
-        build is fun. Both coding and music have allowed me to meet and work with some unique, amazing, 
-        inspiring people.
-    </p>
+    <div class="about">
+        <p>
+            I am a full-stack developer and a music educator. I enjoy problem-solving and breaking down 
+            information into manageable parts to benefit others and myself. I believe life-long learning 
+            is essential to working a job and for living a rewarding life in general. Most recently I have 
+            been learning the Django web framework as well as bass guitar. As a 17-year educator, I have a 
+            wealth of experience in planning, collaborating with others, and mentoring.
+        </p>
+        <p>
+            I am proficient in Python (Flask and Django), Java (Spring Boot), and JavaScript (React) and have 
+            two music degrees. I really enjoy both disciplines as they blend together problem-solving, creativity, 
+            logic, and syntax. Whether working with front-end or back-end, or writing or rehearsing a song, the 
+            build is fun. Both coding and music have allowed me to meet and work with some unique, amazing, 
+            inspiring people.
+        </p>
+    </div>
 
 </template>
 
 <style>
     .about {
-        color: black;
+        background-color: rgb(24, 18, 18);
+        color: white;
+        border: 20px solid black;
+        border-radius: 8px;
+        max-width: 800px;
+        box-shadow: 0px 0px 10px black;
     }
 </style>
