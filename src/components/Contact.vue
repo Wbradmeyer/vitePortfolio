52a8a28005f24e8eafef1d844118da1eef67e437
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-26 14:06:27 -0500

    contact widen reset

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -27,10 +27,6 @@
         justify-content: center;
         align-items: center;
         padding: 20px;
-        background-color: rgb(24, 18, 18);
-        color: white;
-        border-radius: 8px;
-        max-width: 450px;
     }
 
     .card {
@@ -41,10 +37,10 @@
     form {
         margin: 0px;
         text-align: left;
-        /* background-color: rgb(24, 18, 18);
+        background-color: rgb(24, 18, 18);
         color: white;
         border-radius: 8px;
-        max-width: 400px; */
+        max-width: 450px;
         padding: 10px;
         box-shadow: 0px 0px 10px black;
     }
