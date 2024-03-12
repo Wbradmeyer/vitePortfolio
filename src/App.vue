e1c2510f0ec1dbc699aef2f4a205bbdaedb89372
W. Brad Meyer <wbradmeyer@gmail.com>
2024-03-11 21:46:19 -0400

    reset

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,6 @@ import Projects from './components/Projects.vue';
 import Contact from './components/Contact.vue';
 </script>
 
-<script src="assets/three.r134.min.js"></script>
-<script src="assets/vanta.birds.min.js"></script>
-<script>
-import Vanta from 'vanta;'
-
-export default {
-  mounted() {
-    VANTA.BIRDS({
-      el: "body",
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 200.00,
-      minWidth: 200.00,
-      scale: 1.00,
-      scaleMobile: 1.00
-    })
-  }
-}
-</script>
 
 <template>
   <NavBar />
