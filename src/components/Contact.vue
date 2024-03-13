7640a38fe8fa07f183e4f4a36904c37edc371d2d
W. Brad Meyer <wbradmeyer@gmail.com>
2024-03-13 14:43:54 -0400

    img smaller 2

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -9,7 +9,7 @@
                 <a href="mailto:wbradmeyer@gmail.com">Email Me</a>
             </p>
             <p>
-                <img src="../assets/linkedin2.png" alt="linkedin icon" style="border-radius: 12px; max-width: 75px;">
+                <img src="../assets/linkedin2.png" alt="linkedin icon" style="border-radius: 12px; max-width: 73px; margin-left: 2px;">
                 <a href="https://www.linkedin.com/in/wbrad-meyer/">LinkedIn</a>
             </p>
             <p>
