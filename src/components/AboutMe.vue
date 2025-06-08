3b521f8a3c57b6da872a59e12b6273083cd3b488
W. Brad Meyer <wbradmeyer@gmail.com>
2025-06-07 22:14:24 -0400

    fix formatting of paragraph strings in AboutMe section for consistency

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -6,12 +6,14 @@ const paragraph1 = `I am a full-stack developer and a music educator. I enjoy \
     others and myself. I believe life-long learning is essential to working a job \
     and for living a rewarding life in general. Most recently I have been taking \
     the GitHub Foundations course through Microsoft Learn. As an 18-year educator, \
-    I have a wealth of experience in planning, collaborating with others, and mentoring.`
+    I have a wealth of experience in planning, collaborating with others, and mentoring. \
+    `
 const paragraph2 = `I am proficient in Python (Flask and Django), Java (Spring Boot), and \
     JavaScript (React) and have two music degrees. I really enjoy both disciplines as they \
     blend together problem-solving, creativity, logic, and syntax. Whether working with \
     front-end or back-end, or writing or rehearsing a song, the build is fun. Both coding \ 
-    and music have allowed me to meet and work with some unique, amazing, inspiring people.`
+    and music have allowed me to meet and work with some unique, amazing, inspiring people. \
+    `
 
 const displayed1 = ref('')
 const displayed2 = ref('')
